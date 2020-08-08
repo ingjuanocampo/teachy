58a0927f31f12842b652fe3add94663f096c5294
Juan Manuel Ocampo Valencia <juano@hourly.app>
2020-08-08 11:36:27 -0500

    Refactor name

--- app/src/main/java/com/ingjuanocampo/sazonapp/ui/explore/ExploreFragment.kt
+++ app/src/main/java/com/ingjuanocampo/sazonapp/ui/explore/ExploreFragment.kt
@@ -1,4 +1,4 @@
-package com.ingjuanocampo.sazonapp.ui.notifications
+package com.ingjuanocampo.sazonapp.ui.explore
 
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -10,20 +10,20 @@ import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import com.ingjuanocampo.sazonapp.R
 
-class NotificationsFragment : Fragment() {
+class ExploreFragment : Fragment() {
 
-    private lateinit var notificationsViewModel: NotificationsViewModel
+    private lateinit var dashboardViewModel: ExploreViewModel
 
     override fun onCreateView(
             inflater: LayoutInflater,
             container: ViewGroup?,
             savedInstanceState: Bundle?
     ): View? {
-        notificationsViewModel =
-                ViewModelProviders.of(this).get(NotificationsViewModel::class.java)
-        val root = inflater.inflate(R.layout.fragment_notifications, container, false)
-        val textView: TextView = root.findViewById(R.id.text_notifications)
-        notificationsViewModel.text.observe(viewLifecycleOwner, Observer {
+        dashboardViewModel =
+                ViewModelProviders.of(this).get(ExploreViewModel::class.java)
+        val root = inflater.inflate(R.layout.fragment_dashboard, container, false)
+        val textView: TextView = root.findViewById(R.id.text_dashboard)
+        dashboardViewModel.text.observe(viewLifecycleOwner, Observer {
             textView.text = it
         })
         return root
